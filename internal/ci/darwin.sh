14bfc3ea8c371faebfe444391304f7bfb80595a6
therecipe <therecipe@users.noreply.github.com>
2016-10-05 14:10:21 +0200

    trigger travis

--- internal/ci/darwin.sh
+++ internal/ci/darwin.sh
@@ -3,9 +3,9 @@ set -ev
 
 #download and install qt
 curl -sL -o /tmp/qt-opensource-mac-x64-android-ios-5.7.0.dmg https://download.qt.io/official_releases/qt/5.7/5.7.0/qt-opensource-mac-x64-android-ios-5.7.0.dmg
-hdiutil attach -quiet /tmp/qt-opensource-mac-x64-android-ios-5.7.0.dmg
+hdiutil attach -noverify -noautofsck /tmp/qt-opensource-mac-x64-android-ios-5.7.0.dmg
 /Volumes/qt-opensource-mac-x64-android-ios-5.7.0/qt-opensource-mac-x64-android-ios-5.7.0.app/Contents/MacOS/qt-opensource-mac-x64-android-ios-5.7.0 --script $GOPATH/src/github.com/therecipe/qt/internal/ci/iscript.qs
-diskutil unmountDisk disk2
+#diskutil unmountDisk disk2
 
 #download and install android sdk
 curl -sL -o /tmp/android-sdk_r24.4.1-macosx.zip https://dl.google.com/android/android-sdk_r24.4.1-macosx.zip
@@ -21,12 +21,12 @@ export ANDROID_NDK_DIR=/tmp/android-ndk-r12b
 
 #download and install virtualbox
 curl -sL -o /tmp/VirtualBox-5.1.6-110634-OSX.dmg http://download.virtualbox.org/virtualbox/5.1.6/VirtualBox-5.1.6-110634-OSX.dmg
-hdiutil attach -quiet /tmp/VirtualBox-5.1.6-110634-OSX.dmg
+hdiutil attach -noverify -noautofsck -quiet /tmp/VirtualBox-5.1.6-110634-OSX.dmg
 sudo installer -pkg VirtualBox.pkg -target /
-diskutil unmountDisk disk2
+#diskutil unmountDisk disk2
 
 #download and install sailfish sdk
 curl -sL -o /tmp/SailfishOSSDK-Beta-1608-Qt5-mac-offline.dmg https://releases.sailfishos.org/sdk/installers/1608/SailfishOSSDK-Beta-1608-Qt5-mac-offline.dmg
-hdiutil attach -quiet /tmp/SailfishOSSDK-Beta-1608-Qt5-mac-offline.dmg
+hdiutil attach -noverify -noautofsck -quiet /tmp/SailfishOSSDK-Beta-1608-Qt5-mac-offline.dmg
 /Volumes/SailfishOSSDK-mac-offline-160801/SailfishOSSDK-mac-offline-160801.app/Contents/MacOS/SailfishOSSDK-mac-offline-160801 --script $GOPATH/src/github.com/therecipe/qt/internal/ci/iscriptcd.qs
-diskutil unmountDisk disk2
+#diskutil unmountDisk disk2
