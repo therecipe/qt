9a2aa0c77ac9913feee1c0a8b8b13de41c486654
therecipe <therecipe@users.noreply.github.com>
2016-10-05 14:37:43 +0200

    trigger travis

--- internal/ci/darwin.sh
+++ internal/ci/darwin.sh
@@ -3,30 +3,36 @@ set -ev
 
 #download and install qt
 curl -sL -o /tmp/qt-opensource-mac-x64-android-ios-5.7.0.dmg https://download.qt.io/official_releases/qt/5.7/5.7.0/qt-opensource-mac-x64-android-ios-5.7.0.dmg
-hdiutil attach -noverify -noautofsck /tmp/qt-opensource-mac-x64-android-ios-5.7.0.dmg
+hdiutil attach -noverify -noautofsck -quiet /tmp/qt-opensource-mac-x64-android-ios-5.7.0.dmg
 /Volumes/qt-opensource-mac-x64-android-ios-5.7.0/qt-opensource-mac-x64-android-ios-5.7.0.app/Contents/MacOS/qt-opensource-mac-x64-android-ios-5.7.0 --script $GOPATH/src/github.com/therecipe/qt/internal/ci/iscript.qs
-#diskutil unmountDisk disk2
+diskutil unmountDisk disk1
+rm -f /tmp/qt-opensource-mac-x64-android-ios-5.7.0.dmg
 
 #download and install android sdk
 curl -sL -o /tmp/android-sdk_r24.4.1-macosx.zip https://dl.google.com/android/android-sdk_r24.4.1-macosx.zip
 unzip -qq /tmp/android-sdk_r24.4.1-macosx.zip -d /tmp
 export ANDROID_SDK_DIR=/tmp/android-sdk-macosx
+rm -f /tmp/android-sdk_r24.4.1-macosx.zip
 
 #install deps for android sdk
+echo "y" | $ANDROID_SDK_DIR/tools/android -s update sdk -f -u -a -t 1,2,4,31
 
 #download and install android ndk
 curl -sL -o /tmp/android-ndk-r12b-darwin-x86_64.zip https://dl.google.com/android/repository/android-ndk-r12b-darwin-x86_64.zip
 unzip -qq /tmp/android-ndk-r12b-darwin-x86_64.zip -d /tmp
 export ANDROID_NDK_DIR=/tmp/android-ndk-r12b
+rm -f /tmp/android-ndk-r12b-darwin-x86_64.zip
 
 #download and install virtualbox
 curl -sL -o /tmp/VirtualBox-5.1.6-110634-OSX.dmg http://download.virtualbox.org/virtualbox/5.1.6/VirtualBox-5.1.6-110634-OSX.dmg
 hdiutil attach -noverify -noautofsck -quiet /tmp/VirtualBox-5.1.6-110634-OSX.dmg
-sudo installer -pkg VirtualBox.pkg -target /
-#diskutil unmountDisk disk2
+sudo installer -pkg /Volumes/VirtualBox/VirtualBox.pkg -target /
+diskutil unmountDisk disk1
+rm -f /tmp/VirtualBox-5.1.6-110634-OSX.dmg
 
 #download and install sailfish sdk
 curl -sL -o /tmp/SailfishOSSDK-Beta-1608-Qt5-mac-offline.dmg https://releases.sailfishos.org/sdk/installers/1608/SailfishOSSDK-Beta-1608-Qt5-mac-offline.dmg
 hdiutil attach -noverify -noautofsck -quiet /tmp/SailfishOSSDK-Beta-1608-Qt5-mac-offline.dmg
 /Volumes/SailfishOSSDK-mac-offline-160801/SailfishOSSDK-mac-offline-160801.app/Contents/MacOS/SailfishOSSDK-mac-offline-160801 --script $GOPATH/src/github.com/therecipe/qt/internal/ci/iscriptcd.qs
-#diskutil unmountDisk disk2
+diskutil unmountDisk disk1
+rm -f /tmp/SailfishOSSDK-Beta-1608-Qt5-mac-offline.dmg
--- internal/ci/linux.sh
+++ internal/ci/linux.sh
@@ -12,26 +12,32 @@ sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 90
 curl -sL -o /tmp/qt-opensource-linux-x64-android-5.7.0.run https://download.qt.io/official_releases/qt/5.7/5.7.0/qt-opensource-linux-x64-android-5.7.0.run
 chmod +x /tmp/qt-opensource-linux-x64-android-5.7.0.run
 /tmp/qt-opensource-linux-x64-android-5.7.0.run --script $GOPATH/src/github.com/therecipe/qt/internal/ci/iscript.qs
+rm -f /tmp/qt-opensource-linux-x64-android-5.7.0.run
 
 #download and install android sdk
 curl -sL -o /tmp/android-sdk_r24.4.1-linux.tgz https://dl.google.com/android/android-sdk_r24.4.1-linux.tgz
 tar -xzf /tmp/android-sdk_r24.4.1-linux.tgz -C /tmp
 export ANDROID_SDK_DIR=/tmp/android-sdk-linux
+rm -f /tmp/android-sdk_r24.4.1-linux.tgz
 
 #install deps for android sdk
+echo "y" | $ANDROID_SDK_DIR/tools/android -s update sdk -f -u -a -t 1,2,4,31
 
 #download and install android ndk
 curl -sL -o /tmp/android-ndk-r12b-linux-x86_64.zip https://dl.google.com/android/repository/android-ndk-r12b-linux-x86_64.zip
 unzip -qq /tmp/android-ndk-r12b-linux-x86_64.zip -d /tmp
 export ANDROID_NDK_DIR=/tmp/android-ndk-r12b
+rm -f /tmp/android-ndk-r12b-linux-x86_64.zip
 
 #download and install virtualbox
 sudo apt-get -y -qq install linux-headers-generic
 curl -sL -o /tmp/VirtualBox-5.1.6-110634-Linux_amd64.run http://download.virtualbox.org/virtualbox/5.1.6/VirtualBox-5.1.6-110634-Linux_amd64.run
 chmod +x /tmp/VirtualBox-5.1.6-110634-Linux_amd64.run
 sudo /tmp/VirtualBox-5.1.6-110634-Linux_amd64.run
+rm -f /tmp/VirtualBox-5.1.6-110634-Linux_amd64.run
 
 #download and install sailfish sdk
 curl -sL -o /tmp/SailfishOSSDK-Beta-1608-Qt5-linux-64-offline.run https://releases.sailfishos.org/sdk/installers/1608/SailfishOSSDK-Beta-1608-Qt5-linux-64-offline.run
 chmod +x /tmp/SailfishOSSDK-Beta-1608-Qt5-linux-64-offline.run
 /tmp/SailfishOSSDK-Beta-1608-Qt5-linux-64-offline.run --script $GOPATH/src/github.com/therecipe/qt/internal/ci/iscript.qs
+rm -f /tmp/SailfishOSSDK-Beta-1608-Qt5-linux-64-offline.run
