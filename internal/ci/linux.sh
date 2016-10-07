475bc07013d9a9dbf93b04e9d18e53a1ec88f8c6
therecipe <therecipe@users.noreply.github.com>
2016-10-07 18:03:28 +0200

    trigger circleci

--- internal/ci/linux.sh
+++ internal/ci/linux.sh
@@ -19,14 +19,14 @@ sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 90
 
 #download and install qt
 QT=qt-opensource-linux-x64-android-5.7.0.run
-curl -sL --retry 3 -o /tmp/$QT https://download.qt.io/official_releases/qt/5.7/5.7.0/$QT
+curl -sL --retry 6 --retry-delay 20 -o /tmp/$QT https://download.qt.io/official_releases/qt/5.7/5.7.0/$QT
 chmod +x /tmp/$QT
 /tmp/$QT --script $GOPATH/src/github.com/therecipe/qt/internal/ci/iscript.qs
 rm -f /tmp/$QT
 
 #download and install android sdk
 SDK=android-sdk_r24.4.1-linux.tgz
-curl -sL --retry 3 -o /tmp/$SDK https://dl.google.com/android/$SDK
+curl -sL --retry 6 --retry-delay 20 -o /tmp/$SDK https://dl.google.com/android/$SDK
 tar -xzf /tmp/$SDK -C $HOME
 rm -f /tmp/$SDK
 
@@ -35,7 +35,7 @@ echo "y" | $HOME/android-sdk-linux/tools/android -s update sdk -f -u -a -t 1,2,4
 
 #download and install android ndk
 NDK=android-ndk-r12b-linux-x86_64.zip
-curl -sL --retry 3 -o /tmp/$NDK https://dl.google.com/android/repository/$NDK
+curl -sL --retry 6 --retry-delay 20 -o /tmp/$NDK https://dl.google.com/android/repository/$NDK
 unzip -qq /tmp/$NDK -d $HOME
 rm -f /tmp/$NDK
 
