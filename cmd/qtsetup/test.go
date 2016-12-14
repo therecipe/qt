8ab8ca9dea55f0f85ce6234ad76b880c83f7c4f6
Jordan Orelli <jordanorelli@gmail.com>
2016-12-14 09:52:54 -0600

    clean up example config

--- cmd/qtsetup/test.go
+++ cmd/qtsetup/test.go
@@ -9,17 +9,41 @@ import (
 	"github.com/therecipe/qt/internal/utils"
 )
 
+var exampleApps = []struct {
+	path []string
+}{
+	{path: []string{"widgets", "line_edits"}},
+	{path: []string{"widgets", "video_player"}},
+	{path: []string{"widgets", "graphicsscene"}},
+	{path: []string{"widgets", "dropsite"}},
+	{path: []string{"widgets", "table"}},
+	{path: []string{"widgets", "treeview", "treeview_dual"}},
+	{path: []string{"widgets", "treeview", "treeview_filelist"}},
+	{path: []string{"widgets", "bridge2"}},
+	{path: []string{"widgets", "systray"}},
+	{path: []string{"widgets", "renderer"}},
+	{path: []string{"widgets", "textedit"}},
+	{path: []string{"quick", "bridge"}},
+	{path: []string{"quick", "bridge2"}},
+	{path: []string{"quick", "calc"}},
+	{path: []string{"quick", "dialog"}},
+	{path: []string{"quick", "sailfish"}},
+	{path: []string{"quick", "translate"}},
+	{path: []string{"quick", "view"}},
+	{path: []string{"quick", "tableview"}},
+	{path: []string{"quick", "dynamic"}},
+	{path: []string{"qml", "application"}},
+	{path: []string{"qml", "material"}},
+	{path: []string{"qml", "prop"}},
+	{path: []string{"uitools", "calculator"}},
+}
+
 func test(buildTarget string) {
 	utils.Log.Infof("running setup/test %v (~10min)", buildTarget)
 
 	//TODO: cleanup
-	for _, example := range []string{filepath.Join("widgets", "line_edits"), filepath.Join("widgets", "video_player"), filepath.Join("widgets", "graphicsscene"), filepath.Join("widgets", "dropsite"), filepath.Join("widgets", "table"),
-		filepath.Join("widgets", "treeview", "treeview_dual"), filepath.Join("widgets", "treeview", "treeview_filelist"), filepath.Join("widgets", "bridge2"), filepath.Join("widgets", "systray"), filepath.Join("widgets", "renderer"),
-		filepath.Join("widgets", "textedit"),
-		filepath.Join("quick", "bridge"), filepath.Join("quick", "bridge2"), filepath.Join("quick", "calc"), filepath.Join("quick", "dialog"), filepath.Join("quick", "sailfish"), filepath.Join("quick", "translate"), filepath.Join("quick", "view"),
-		filepath.Join("quick", "tableview"), filepath.Join("quick", "dynamic"),
-		filepath.Join("qml", "application"), filepath.Join("qml", "material"), filepath.Join("qml", "prop"),
-		filepath.Join("uitools", "calculator")} {
+	for _, app := range exampleApps {
+		example := filepath.Join(app.path...)
 
 		if (buildTarget == "sailfish" || buildTarget == "sailfish-emulator") && (!strings.Contains(example, "quick") || (example == filepath.Join("quick", "dynamic") || example == filepath.Join("quick", "tableview") || example == filepath.Join("quick", "bridge") || example == filepath.Join("quick", "dialog"))) {
 		} else if !(buildTarget == "sailfish" || buildTarget == "sailfish-emulator") && example == filepath.Join("quick", "sailfish") {
