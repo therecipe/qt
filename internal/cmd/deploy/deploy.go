ea239838557d4484d590b429f6d90d9a90de6b43
therecipe <therecipe@users.noreply.github.com>
2017-01-09 22:09:05 +0100

    multiple gopath support for qtdeploy with docker

--- internal/cmd/deploy/deploy.go
+++ internal/cmd/deploy/deploy.go
@@ -990,11 +990,36 @@ func deployDocker() {
 		}
 	}
 
-	if !strings.Contains(appPath, utils.MustGoPath()) {
+	var (
+		args = []string{"run", "--rm"}
+		path = make([]string, 0)
+	)
+
+	for i, gp := range strings.Split(os.Getenv("GOPATH"), string(filepath.ListSeparator)) {
+		args = append(args, []string{"-v", fmt.Sprintf("%v:/media/sf_GOPATH%v", gp, i)}...)
+		path = append(path, fmt.Sprintf("/media/sf_GOPATH%v", i))
+	}
+
+	args = append(args, []string{"-e", "GOPATH=" + strings.Join(path, ":")}...)
+
+	args = append(args, []string{"-i", fmt.Sprintf("therecipe/qt:%v", dockerImage)}...)
+
+	args = append(args, []string{"qtdeploy", "build", buildTarget}...)
+
+	var found bool
+	for i, gp := range strings.Split(os.Getenv("GOPATH"), string(filepath.ListSeparator)) {
+		if strings.HasPrefix(appPath, gp) {
+			args = append(args, strings.Replace(strings.Replace(appPath, gp, fmt.Sprintf("/media/sf_GOPATH%v", i), -1), "\\", "/", -1))
+			found = true
+			break
+		}
+	}
+
+	if !found {
 		utils.Log.Panicln("Project needs to be inside GOPATH", appPath, utils.MustGoPath())
 	}
 
-	utils.RunCmd(exec.Command("docker", "run", "--rm", "-v", fmt.Sprintf("%v:/media/sf_GOPATH", utils.MustGoPath()), "-i", fmt.Sprintf("therecipe/qt:%v", dockerImage), "qtdeploy", "build", buildTarget, strings.Replace(strings.Replace(appPath, utils.MustGoPath(), "/media/sf_GOPATH", -1), "\\", "/", -1)), fmt.Sprintf("deploy binary for %v on %v with docker", buildTarget, runtime.GOOS))
+	utils.RunCmd(exec.Command("docker", args...), fmt.Sprintf("deploy binary for %v on %v with docker", buildTarget, runtime.GOOS))
 }
 
 func run() {
