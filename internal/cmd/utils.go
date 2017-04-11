bc5bdbc293ee913353da7b043d88c21176d43f54
therecipe <therecipe@users.noreply.github.com>
2017-04-11 21:07:16 +0200

    make qtdeploy faster by filtering out std packages

--- internal/cmd/utils.go
+++ internal/cmd/utils.go
@@ -10,8 +10,14 @@ import (
 	"github.com/therecipe/qt/internal/utils"
 )
 
+var std []string
+
 func GetImports(path string, level int) []string {
 	utils.Log.WithField("path", path).WithField("level", level).Debug("imports")
+
+	if std == nil {
+		std = strings.Split(strings.TrimSpace(utils.RunCmd(exec.Command("go", "list", "std"), "go list std")), "\n")
+	}
 
 	importMap := make(map[string]struct{})
 
@@ -21,9 +27,19 @@ func GetImports(path string, level int) []string {
 	out := strings.TrimSpace(utils.RunCmd(cmd, "go list deps"))
 	out = strings.Replace(out, "'", "", -1)
 	out = strings.Replace(out, ":", "|", -1)
+	libs := strings.Split(out, "|")
+
+	for i := len(libs) - 1; i >= 0; i-- {
+		for _, k := range std {
+			if libs[i] == k {
+				libs = append(libs[:i], libs[i+1:]...)
+				break
+			}
+		}
+	}
 
 	cmd = exec.Command("go", "list", "-e", "-f", "{{.Dir}}")
-	cmd.Args = append(cmd.Args, strings.Split(out, "|")...)
+	cmd.Args = append(cmd.Args, libs...)
 	cmd.Dir = path
 
 	for _, dep := range strings.Split(strings.TrimSpace(utils.RunCmd(cmd, "go list dir")), "\n") {
@@ -39,10 +55,8 @@ func GetImports(path string, level int) []string {
 	}
 
 	var imports []string
-
 	for k := range importMap {
 		imports = append(imports, k)
 	}
-
 	return imports
 }
