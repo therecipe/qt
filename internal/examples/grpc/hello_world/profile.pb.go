ce1a37096c30e4ab257512bc0a6f18026164f6e7
Kenneth Shaw <kenshaw@gmail.com>
2017-04-12 16:03:39 +0700

    Fixing import paths for grpc-ecosystem

--- internal/examples/grpc/hello_world/profile.pb.go
+++ internal/examples/grpc/hello_world/profile.pb.go
@@ -7,7 +7,7 @@ package main
 import proto "github.com/golang/protobuf/proto"
 import fmt "fmt"
 import math "math"
-import _ "github.com/gengo/grpc-gateway/third_party/googleapis/google/api"
+import _ "github.com/grpc-ecosystem/grpc-gateway/third_party/googleapis/google/api"
 
 import (
 	context "golang.org/x/net/context"
