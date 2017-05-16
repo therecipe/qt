a5df3240752dcd861fecb60feae29e559e775886
therecipe <therecipe@users.noreply.github.com>
2017-05-16 17:59:34 +0200

    fix qtsetup update

--- internal/cmd/setup/update.go
+++ internal/cmd/setup/update.go
@@ -15,15 +15,15 @@ func Update() {
 	utils.RunCmd(exec.Command("go", "clean", "-i", "github.com/therecipe/qt/cmd/..."), "run \"go clean cmd\"")
 	utils.RunCmd(exec.Command("go", "clean", "-i", "github.com/therecipe/qt/internal/..."), "run \"go clean internal\"")
 
-	fetch := exec.Command("git", "fetch", "--all")
+	fetch := exec.Command("git", "fetch", "-f", "--all")
 	fetch.Dir = filepath.Join(utils.MustGoPath(), "src", "github.com", "therecipe", "qt")
 	utils.RunCmd(fetch, "run \"git fetch\"")
 
-	checkoutCmd := exec.Command("git", "checkout", "--", utils.GoQtPkgPath("cmd"))
+	checkoutCmd := exec.Command("git", "checkout", "-f", "--", utils.GoQtPkgPath("cmd"))
 	checkoutCmd.Dir = filepath.Join(utils.MustGoPath(), "src", "github.com", "therecipe", "qt")
 	utils.RunCmd(checkoutCmd, "run \"git checkout cmd\"")
 
-	checkoutInternal := exec.Command("git", "checkout", "--", utils.GoQtPkgPath("internal"))
+	checkoutInternal := exec.Command("git", "checkout", "-f", "--", utils.GoQtPkgPath("internal"))
 	checkoutInternal.Dir = filepath.Join(utils.MustGoPath(), "src", "github.com", "therecipe", "qt")
 	utils.RunCmd(checkoutInternal, "run \"git checkout internal\"")
 
@@ -45,7 +45,7 @@ func Upgrade() {
 	utils.RunCmd(exec.Command("go", "clean", "-i", "github.com/therecipe/qt/..."), "run \"go clean\"")
 	utils.RemoveAll(utils.GoQtPkgPath())
 
-	utils.RunCmd(exec.Command("go", "get", "-v", "-d", "github.com/therecipe/qt"), "run \"go get\"")
+	utils.RunCmd(exec.Command("go", "get", "-v", "-d", "github.com/therecipe/qt/cmd/..."), "run \"go get\"")
 
 	hash := "please install git"
 	if _, err := exec.LookPath("git"); err == nil {
