f5eecc27111cea17e0e9ed2a645ad0ac3c96a9fb
therecipe <therecipe@users.noreply.github.com>
2017-05-25 20:46:28 +0200

    new qml/extending examples

---
+++ internal/examples/qml/extending/components/test_qml/component/SomeComponent.qml
@@ -0,0 +1,7 @@
+import QtQuick 2.0
+
+Rectangle {
+    width: 300; height: 200
+
+    color: "blue"
+}
---
+++ internal/examples/qml/extending/components/test_qml/main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"os"
+
+	"github.com/therecipe/qt/core"
+	"github.com/therecipe/qt/gui"
+	"github.com/therecipe/qt/quick"
+
+	_ "github.com/therecipe/qt/internal/examples/qml/extending/components/test_qml/component"
+)
+
+func main() {
+	gui.NewQGuiApplication(len(os.Args), os.Args)
+
+	view := quick.NewQQuickView(nil)
+	view.SetResizeMode(quick.QQuickView__SizeRootObjectToView)
+	view.SetSource(core.NewQUrl3("qrc:///app.qml", 0))
+	view.Show()
+
+	gui.QGuiApplication_Exec()
+}
