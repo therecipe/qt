therecipe/qt

commit c38504e5a84f015d256affe7f449b36f2a5bc4e3

fix qtmoc namespace issues + fix ubports issues

internal/binding/templater/template_cpp.go
@@ -551,6 +551,12 @@ func preambleCpp(module string, input []byte, mode int, tags string) []byte {
 				}
 			}
 
+			if c, ok := parser.State.ClassMap[class]; ok {
+				if strings.Contains(c.Pkg, "/vendor/") {
+					continue
+				}
+			}
+
 			fmt.Fprintf(bb, "#include <%v>\n", class)
 
 			if mode == MOC {
@@ -607,6 +613,25 @@ func preambleCpp(module string, input []byte, mode int, tags string) []byte {
 	fmt.Fprint(bb, "\n")
 
 	bb.Write(input)
+
+	//TODO: regexp
+	if mode == MOC {
+		pre := bb.String()
+		bb.Reset()
+		for _, c := range parser.SortedClassesForModule(module, true) {
+			hName := c.Hash()
+			sep := []string{" ", "\t", "\n", "\r", "(", ")", ":", ";", "*", "<", ">", "&", "~", "{", "}", "[", "]", "_", "callback"}
+			for _, p := range sep {
+				for _, s := range sep {
+					if s == "callback" {
+						continue
+					}
+					pre = strings.Replace(pre, p+c.Name+s, p+c.Name+hName+s, -1)
+				}
+			}
+		}
+		bb.WriteString(pre)
+	}
 
 	return bb.Bytes()
 }

internal/binding/templater/template_go.go
@@ -300,7 +300,7 @@ import "C"
 	if mode == MOC {
 		for _, lib := range parser.GetLibs() {
 			mlow := strings.ToLower(lib)
-			for _, pre := range []string{" ", "\t", "\r", "\n", "!", "*", "(", ")"} {
+			for _, pre := range []string{" ", "\t", "\r", "\n", "!", "*", "(", ")", "[", "]"} {
 				for _, past := range []string{"NewQ", "PointerFromQ", "Q"} {
 					inputString = strings.Replace(inputString, fmt.Sprintf("%v%v.%v", pre, mlow, past), fmt.Sprintf("%vstd_%v.%v", pre, mlow, past), -1)
 				}
@@ -385,30 +385,54 @@ import "C"
 		}
 	}
 
-	for custom, m := range parser.GetCustomLibs(target, tags) {
-		mlows := strings.Split(m, "/")
-		mlow := mlows[len(mlows)-1]
-		switch {
-		case strings.Contains(inputString, fmt.Sprintf("custom_%v.", mlow)):
-			fmt.Fprintf(bb, "custom_%v \"%v\"\n", mlow, m)
-		case strings.Contains(inputString, fmt.Sprintf("%v.", custom)):
-			fmt.Fprintf(bb, "%v \"%v\"\n", custom, m)
+	if mode == MOC {
+		for custom, m := range parser.GetCustomLibs(target, tags) {
+			switch {
+			case strings.Contains(m, "/vendor/"):
+				fmt.Fprintf(bb, "\"%v\"\n", custom)
+
+			case strings.Contains(inputString, fmt.Sprintf("%v.", custom)):
+				fmt.Fprintf(bb, "%v \"%v\"\n", custom, m)
+			}
+		}
+
+		for i := range parser.State.MocImports {
+			fmt.Fprintf(bb, "%v\n", i)
+
+			if strings.HasPrefix(i, ".") {
+				delete(parser.State.MocImports, i)
+			}
 		}
 	}
-
-	for i := range parser.State.MocImports {
-		fmt.Fprintf(bb, "%v\n", i)
-	}
-	parser.State.MocImports = make(map[string]struct{})
 
 	fmt.Fprintln(bb, ")")
 
 	bb.WriteString(inputString)
 
-	var out, err = format.Source(renameSubClasses(bb.Bytes(), "_"))
+	out, err := format.Source(renameSubClasses(bb.Bytes(), "_"))
 	if err != nil {
-		utils.Log.WithError(err).Panicln("failed to format:", module)
+		utils.Log.WithError(err).Errorln("failed to format:", pkg, module)
+		out = bb.Bytes()
 	}
+
+	//TODO: regexp
+	if mode == MOC {
+		pre := string(out)
+		for _, c := range parser.SortedClassesForModule(module, true) {
+			hName := c.Hash()
+			sep := []string{"\n", "(", "_", "callback", "C."}
+			for _, p := range sep {
+				for _, s := range sep {
+					if s == "callback" || s == "C." {
+						continue
+					}
+					pre = strings.Replace(pre, p+c.Name+s, p+c.Name+hName+s, -1)
+				}
+			}
+		}
+		out = []byte(pre)
+	}
+
 	return out
 }
 

internal/binding/templater/template_h.go
@@ -53,5 +53,24 @@ func HTemplate(m string, mode int, tags string) []byte {
 	//footer
 	fmt.Fprint(bb, "\n#ifdef __cplusplus\n}\n#endif\n\n#endif")
 
+	//TODO: regexp
+	if mode == MOC {
+		pre := bb.String()
+		bb.Reset()
+		for _, c := range parser.SortedClassesForModule(m, true) {
+			hName := c.Hash()
+			sep := []string{" ", "\t", "\n", "\r", "(", ")", ":", ";", "*", "<", ">", "&", "~", "{", "}", "[", "]", "_", "callback"}
+			for _, p := range sep {
+				for _, s := range sep {
+					if s == "callback" {
+						continue
+					}
+					pre = strings.Replace(pre, p+c.Name+s, p+c.Name+hName+s, -1)
+				}
+			}
+		}
+		bb.WriteString(pre)
+	}
+
 	return bb.Bytes()
 }

commit f1d82edc122bc2fae734ef735cccc74ff6cbed6e

fix qtmoc property regression introduced with c38504e

internal/binding/templater/template_cpp.go
@@ -110,7 +110,7 @@ func CppTemplate(module string, mode int, target, tags string) []byte {
 									ty = fmt.Sprintf("type%v", hex.EncodeToString(tHash.Sum(nil)[:3]))
 								}
 
-								fmt.Fprintf(bb, "Q_PROPERTY(%v %v READ %v WRITE set%v NOTIFY %vChanged)\n", ty, p.Name,
+								fmt.Fprintf(bb, "Q_PROPERTY(%v PREPRO%v READ %v WRITE set%v NOTIFY %vChanged)\n", ty, p.Name,
 									func() string {
 										if p.Output == "bool" && !strings.HasPrefix(strings.ToLower(p.Name), "is") {
 											return "is" + strings.Title(p.Name)
@@ -630,7 +630,20 @@ func preambleCpp(module string, input []byte, mode int, tags string) []byte {
 	if mode == MOC {
 		pre := bb.String()
 		bb.Reset()
-		for _, c := range parser.SortedClassesForModule(module, true) {
+		libsm := make(map[string]struct{}, 0)
+		for _, c := range parser.State.ClassMap {
+			if c.Pkg != "" && c.IsSubClassOfQObject() {
+				libsm[c.Module] = struct{}{}
+			}
+		}
+
+		var libs []string
+		for k := range libsm {
+			libs = append(libs, k)
+		}
+		libs = append(libs, module)
+
+		for _, c := range parser.SortedClassesForModule(strings.Join(libs, ","), true) {
 			hName := c.Hash()
 			sep := []string{" ", "\t", "\n", "\r", "(", ")", ":", ";", "*", "<", ">", "&", "~", "{", "}", "[", "]", "_", "callback"}
 			for _, p := range sep {
@@ -642,6 +655,7 @@ func preambleCpp(module string, input []byte, mode int, tags string) []byte {
 				}
 			}
 		}
+		pre = strings.Replace(pre, "PREPRO", "", -1)
 		bb.WriteString(pre)
 	}
 

internal/binding/templater/template_go.go
@@ -620,7 +620,20 @@ import "C"
 	//TODO: regexp
 	if mode == MOC {
 		pre := string(out)
-		for _, c := range parser.SortedClassesForModule(module, true) {
+		libsm := make(map[string]struct{}, 0)
+		for _, c := range parser.State.ClassMap {
+			if c.Pkg != "" && c.IsSubClassOfQObject() {
+				libsm[c.Module] = struct{}{}
+			}
+		}
+
+		var libs []string
+		for k := range libsm {
+			libs = append(libs, k)
+		}
+		libs = append(libs, module)
+
+		for _, c := range parser.SortedClassesForModule(strings.Join(libs, ","), true) {
 			hName := c.Hash()
 			sep := []string{"\n", "(", "_", "callback", "C."}
 			for _, p := range sep {

internal/binding/templater/template_h.go
@@ -57,7 +57,20 @@ func HTemplate(m string, mode int, tags string) []byte {
 	if mode == MOC {
 		pre := bb.String()
 		bb.Reset()
-		for _, c := range parser.SortedClassesForModule(m, true) {
+		libsm := make(map[string]struct{}, 0)
+		for _, c := range parser.State.ClassMap {
+			if c.Pkg != "" && c.IsSubClassOfQObject() {
+				libsm[c.Module] = struct{}{}
+			}
+		}
+
+		var libs []string
+		for k := range libsm {
+			libs = append(libs, k)
+		}
+		libs = append(libs, m)
+
+		for _, c := range parser.SortedClassesForModule(strings.Join(libs, ","), true) {
 			hName := c.Hash()
 			sep := []string{" ", "\t", "\n", "\r", "(", ")", ":", ";", "*", "<", ">", "&", "~", "{", "}", "[", "]", "_", "callback"}
 			for _, p := range sep {